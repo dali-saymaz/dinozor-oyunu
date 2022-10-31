aae47f94dd74fbd686cc4c27d1149eda5ec1a2de
esra-er <er.esragul@gmail.com>
2022-10-31 21:34:28 +0100

    dino zipliyor

--- script.js
+++ script.js
@@ -3,5 +3,16 @@ let dino= document.getElementById(`trex`);
 let flower= document.querySelector(`#kaktus`);
 
 function zipla() {
-    dino.classList.add(`trex-animate`);
+    if (flower.classList != "kaktus-animate") {
+        flower.classList.add("kaktus-animate")
+    }
+    if (dino.classList != "trex-animate") {
+        dino.classList.add(`trex-animate`);
+        setTimeout(() => {
+            dino.classList.remove(`trex-animate`)
+        }, 500);
+    }
+   
+
 }
+
